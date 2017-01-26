8bebf401747317f733e5c454008db2553c6b7299
imaquantuman <astump.stump@gmail.com>
2017-01-26 10:38:59 -0600

    ReadFile from struct to class

--- ReadFile.h
+++ ReadFile.h
@@ -6,17 +6,24 @@
 #include <fstream>
 using namespace std;
 
-struct ReadFile
+class ReadFile
 {
-   ifstream input_file;
+private:
+	ifstream input_file;
    bool _eof;
    bool closed;
+public:
+	ReadFile(const char* file_name);
+	~ReadFile();
+	String* readLine();
+	void eof();
+	void close();
 };
 
-ReadFile* createReadFile(const char* file_name);
-void destroyReadFile(ReadFile* rf);
-String* readLine(ReadFile* rf);
-bool eof(ReadFile* rf);
-void close(ReadFile* rf);
+//ReadFile* createReadFile(const char* file_name);
+//void destroyReadFile(ReadFile* rf);
+//String* readLine(ReadFile* rf);
+//bool eof(ReadFile* rf);
+//void close(ReadFile* rf);
 
 #endif
